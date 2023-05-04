66a4a43f3f66230b1bec0860d5deeb53e6a95f96
Marc Galindo <marc@galgar.dev>
2023-05-04 10:25:18 +0200

    Renderizado en new tab. Anchors que no estén en índice tienen target_ blank. Carga título del documento en base al H1. Añado estilos al renderizado.

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const STYLE_SHEET_PATH = "./assets/css/style.css";
+const FIRST_UL_SELECTOR = "ul:first-of-type";
+
+function addTargetBlank(document) {
+    const firstUl = document.querySelector(FIRST_UL_SELECTOR);
+    const anchors = document.querySelectorAll(`a:not(${FIRST_UL_SELECTOR} a)`);
+    anchors.forEach((anchor) => {
+        anchor.target = "_blank";
+    });
+}
+
+function render() {
+    const uploadedFile = document.getElementById("uploadedFile").files[0];
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        const content = event.target.result;
+        const html = marked(content);
+        const newTab = window.open("");
+        newTab.document.write(html);
+        newTab.addEventListener("load", function () {
+            // cargar el título
+            const docTitle = newTab.document.querySelector("h1")?.innerText;
+            newTab.document.title = docTitle || "Resultado";
+            // cargar la hoja de estilos
+            const loadStyles = newTab.document.createElement("link");
+            loadStyles.rel = "stylesheet";
+            loadStyles.type = "text/css";
+            loadStyles.href = STYLE_SHEET_PATH;
+            newTab.document.head.appendChild(loadStyles);
+            // agregar el atributo target="_blank" a los enlaces fuera de la primera ul
+            addTargetBlank(newTab.document);
+        });
+        newTab.document.close();
+    };
+    reader.readAsText(uploadedFile);
+}
