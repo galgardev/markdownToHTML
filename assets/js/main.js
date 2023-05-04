0f3a80d0b7bf2db399c2910e56eda35c1fd02293
Marc Galindo <marc@galgar.dev>
2023-05-04 14:26:55 +0200

    Corregidos estilos en etiquetas pre

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ const FIRST_UL_SELECTOR = "ul:first-of-type";
 function addTargetBlank(document) {
     const firstUl = document.querySelector(FIRST_UL_SELECTOR);
     const anchors = document.querySelectorAll(`a:not(${FIRST_UL_SELECTOR} a)`);
+
     anchors.forEach((anchor) => {
         anchor.target = "_blank";
     });
@@ -12,17 +13,22 @@ function addTargetBlank(document) {
 function render() {
     const uploadedFile = document.getElementById("uploadedFile").files[0];
     const reader = new FileReader();
+
     reader.onload = function (event) {
         const content = event.target.result;
         const html = marked(content);
         const newTab = window.open("");
+
         newTab.document.write(html);
         newTab.addEventListener("load", function () {
             // cargar el título
             const docTitle = newTab.document.querySelector("h1")?.innerText;
+
             newTab.document.title = docTitle || "Resultado";
+
             // cargar la hoja de estilos
             const loadStyles = newTab.document.createElement("link");
+
             loadStyles.rel = "stylesheet";
             loadStyles.type = "text/css";
             loadStyles.href = STYLE_SHEET_PATH;
@@ -30,7 +36,9 @@ function render() {
             // agregar el atributo target="_blank" a los enlaces fuera de la primera ul
             addTargetBlank(newTab.document);
         });
+
         newTab.document.close();
     };
+
     reader.readAsText(uploadedFile);
 }
